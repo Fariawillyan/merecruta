064826cda3d14691a07cc397ec99027533e55465
Willyan Faria <fariawillyan@gmail.com>
2022-05-24 20:31:07 -0300

    jdbc auth

--- src/main/java/empresa/rh/merecruta/security/WebSecurityConfig.java
+++ src/main/java/empresa/rh/merecruta/security/WebSecurityConfig.java
@@ -1,21 +1,28 @@
 
 package empresa.rh.merecruta.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSouce;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,19 +42,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    @Bean
+
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        return new InMemoryUserDetailsManager(user);
+        auth.jdbcAuthentication()
+                .dataSource(dataSouce)
+                .passwordEncoder(encoder);
+
     }
-
-
 }
 
